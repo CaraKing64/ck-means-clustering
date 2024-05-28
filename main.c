7f223f0fbaec4edef92070b656c1be143e1fec1b
CaraKing64 <61319081+CaraKing64@users.noreply.github.com>
2024-05-28 18:53:20 +0930

    Initial commit

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+// void ReadFileToArray()
+
+// int Distance(double x1, double y1, double x2, double y2)
+
+// double** GenerateClusters
+// void def AssignToClusters(double* data, double* centroids, int n_centroids)
+// void def MoveClusters()
+
+
+int main(int argc, char** argv){
+
+  char* data_filename = argv[1];
+  if (argc = 1){
+    printf("Enter a filename\n");
+    return 1;
+  }
+  if (argc > 2){
+    printf("Your extra arguments are '");
+    for (int i = 2; i < argc; i++){
+       printf("%s ", argv[i]);
+    }
+    printf("'\n");
+  }
+
+  
+  printf("You have entered the file to do k-means clustering for as %s\n", data_filename);
+
+
+
+
+
+
+
+
+  return 0;
+}

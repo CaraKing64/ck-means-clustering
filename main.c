d839c75cb23e7021e9d0f81c25c5994f01bbb706
CaraKing64 <61319081+CaraKing64@users.noreply.github.com>
2024-05-29 23:19:16 +0930

    just backing stuff up

--- main.c
+++ main.c
@@ -1,12 +1,87 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <time.h>
 
-// void ReadFileToArray()
+int ReadFileToDoubleArray(char* fname, double** end_array, int* p_rows,
+                          int* p_cols) {
+  int rows = *p_rows;
+  int cols = *p_cols;
 
-// int Distance(double x1, double y1, double x2, double y2)
+  // WENDYN via https://stackoverflow.com/questions/12911299/read-csv-file-in-c
+  char str[128];
+  int result;
+  FILE* f = fopen(fname, "r");
+  int count = 0;
+  int comma_count = 0;
+
+  do {
+    result = fscanf(f, "%127[^;\n]", str);
+    if (result == 0) {
+      result = fscanf(f, "%*c");
+    } else if (result == 1) {
+      if (cols == -1) {
+        comma_count = 0;
+        for (int i = 0; i < strlen(str); i++) {
+          if (str[i] == ',') {
+            comma_count++;
+          }
+        }
+      }
+
+      count++;
+    }
+  } while (result != EOF);
+
+  fclose(f);
+
+  rows = count;
+  cols = comma_count + 1;
+  printf("n_values = %d  n_dimensions = %d\n", rows, cols);
+
+  end_array = malloc(sizeof(double*) * rows);
+  if (end_array == NULL) {
+    printf("Memory allocation error.\n");
+    return 1;
+  }
+  for (int r = 0; r < rows; r++) {
+    end_array[r] = malloc(sizeof(double) * cols);
+  }
+
+  FILE* f2 = fopen(fname, "r");
+  result = 0;
+  int row = 0;
+  do {
+    result = fscanf(f2, "%127[^;\n]", str);
+    if (result == 0) {
+      result = fscanf(f2, "%*c");
+    } else if (result == 1) {
+      // printf("%d %s\n", result, str);
+
+      char* token;
+      int i = 0;
+      token = strtok(str, ",");
+      while (token != NULL) {
+        end_array[row][i] = atoi(token);
+        token = strtok(NULL, ",");
+        i++;
+      }
+      row++;
+    }
+  } while (result != EOF);
+  return 0;
+}
+
+double Distance(double* p1, double* p2, int n_vars){
+  double sum;
+  for (int i = 0; i < n_vars; i++){
+    double dist = p1[i] - p2[i];
+    sum += dist*dist;
+  }
+  
+
+}
 
 // double** GenerateClusters
 // void def AssignToClusters(double* data, double* centroids, int n_centroids)
@@ -28,6 +103,8 @@ int main(int argc, char** argv) {
 
   data_filename = argv[1];
 
+  double** data_array;
+
   if (argc > 2) {
     printf("Your extra unused arguments are: ");
     for (int i = 2; i < argc; i++) {
@@ -39,73 +116,18 @@ int main(int argc, char** argv) {
     printf("\n");
   }
 
-  // WENDYN via https://stackoverflow.com/questions/12911299/read-csv-file-in-c
-  char str[128];
-  int result;
-  FILE* f = fopen(data_filename, "r");
-  int count = 0;
-  int comma_count = 0;
-
-  do {
-    result = fscanf(f, "%127[^;\n]", str);
-    if (result == 0) {
-      result = fscanf(f, "%*c");
-    } else if (result == 1) {
-      if (n_dimensions == -1) {
-        comma_count = 0;
-        for (int i = 0; i < strlen(str); i++) {
-          if (str[i] == ',') {
-            comma_count++;
-          }
-        }
-      }
-
-      count++;
-    }
-  } while (result != EOF);
-
-  fclose(f);
-
-  n_values = count;
-  n_dimensions = comma_count + 1;
-  printf("n_values = %d  n_dimensions = %d\n", n_values, n_dimensions);
-
-  double** data_array = malloc(sizeof(double*) * n_values);
-  if (data_array == NULL) {
-    printf("Memory allocation error.\n");
+  int read_value = ReadFileToDoubleArray(data_filename, data_array, &n_values, &n_dimensions);
+  if (read_value == 1){
+    printf("Read file error.\n");
     return 1;
   }
-  for (int r = 0; r < n_values; r++) {
-    data_array[r] = malloc(sizeof(double) * n_dimensions);
-  }
 
-  FILE* f2 = fopen(data_filename, "r");
-  result = 0;
-  int row = 0;
-  do {
-    result = fscanf(f2, "%127[^;\n]", str);
-    if (result == 0) {
-      result = fscanf(f2, "%*c");
-    } else if (result == 1) {
-      //printf("%d %s\n", result, str);
-
-      char* token;
-      int i = 0;
-      token = strtok(str, ",");
-      while (token != NULL) {
-        data_array[row][i] = atoi(token);
-        token = strtok(NULL, ",");
-        i++;
-      }
-      row++;
-
-    }
-  } while (result != EOF);
+  double* origin = {0, 0, 0};
 
   // print all values in the dataset
   for (int r = 0; r < n_values; r++) {
     for (int c = 0; c < n_dimensions; c++) {
-      printf("%.0lf ", data_array[r][c]);
+      printf("%.0lf %.0lf", data_array[r][c], Distance(data_array[1], origin, n_dimensions));
     }
     printf("\n");
   }
